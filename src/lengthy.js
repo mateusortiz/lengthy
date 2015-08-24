a2890f162dec8f0a6be716b56bef7e1c4d5f3a85
Mateus Ortiz <mateus.prado@ig.com.br>
2015-08-23 21:48:46 -0300

    Add amd and factory

--- src/lengthy.js
+++ src/lengthy.js
@@ -5,3 +5,18 @@
 
 	- Inspired by Pretty of John Resig
 */
+
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define([], function() {
+			return factory(root);
+		});
+	} else if (typeof exports === 'object') {
+		module.exports = factory.root();
+	} else {
+		root.lengthy = factory(root);
+	}
+}(this, function (root) {
+	'use strict';
+
+}));
